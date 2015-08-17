60f63b293bf4cc8de0f07d86195ff845e54d620d
Jeff Putnam <jeff.putnam.design@gmail.com>
2015-08-16 18:04:51 -0700

    Trying the php again
    back to basics

--- search.js
+++ search.js
@@ -1,28 +1,3 @@
-//var d = new Date();
-//var seconds = d.getTime() / 1000;
-//
-//oauth_signature =
-//oauth_signature_method = HMAC-SHA1;
-//oauth_timestamp = seconds;
-//oauth_tokenoken = "12615282-GcGhBBLfuZrS4NA3DeFMJ1DMbRskpgfmIlDyqWsy1";
-//oauth_version = "1.0";
-//
-
-$.ajax({
-    url: 'https://api.twitter.com/1.1/trends/place.json',
-    type: 'GET',
-    dataType: 'json',
-    contentType: "application/json",
-    beforeSend: function(xhr) {
-        xhr.setRequestHeader('Authorization', 'OAuth oauth_consumer_key="ehQz7k2MqSsQsyqVvozrAFfhI", oauth_nonce="b8668cf81774410e6c5e9ed68c070e28", oauth_signature="n73THgjKBWGstyN3EOL29UxtAWs%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1439767892", oauth_token="12615282-GcGhBBLfuZrS4NA3DeFMJ1DMbRskpgfmIlDyqWsy1", oauth_version="1.0"')
-    }, success: function(data){
-        alert(data);
-        //process the JSON data etc
-    }
-});
-
-
-
 function onClientLoad() {
     gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
 }
