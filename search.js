a6e72edbfd74a17daab1ea46c2d38f9ffa401366
Jeff Putnam <jeff.putnam.design@gmail.com>
2015-08-19 14:51:36 -0700

    Whoops
    Noticed some errors when I was showing this to a friend…
    
    Had to add quotes around each query item
    
    Twitter errors were not appearing on the page as I was putting them in
    a place that does not exist anymore

--- search.js
+++ search.js
@@ -1,5 +1,5 @@
 trendList = [];
-var youtubeQuery = '';
+youtubeQuery = '';
 
 // Twitter API
 function twitterCall(callback){
@@ -11,19 +11,22 @@ function twitterCall(callback){
             //console.log(response);
             var twitterNumber = response[0].trends.length;
 
+            //console.log(resultNumber);
+
             for (var i = 0; i < twitterNumber; i++) {
                 $('#trends-container').append('<li>' + response[0].trends[i].name + '</li>');
                 var thisTrend = response[0].trends[i].name;
                 var trendString = thisTrend.toString();
 
-                trendList.push(trendString);
+                trendList.push('"' + trendString + '"');
 
             }
             callback();
 
+
         },
         error: function(errors) {
-            $('.tweets-container p:first').text('Request error');
+            $('#trends-container').append('<p>Request error</p>');
             console.log(errors);
         }
 
@@ -32,7 +35,10 @@ function twitterCall(callback){
 }
 
 twitterCall(function() {
+    //console.log('After Twitter API: ' + trendList.toString());
     youtubeQuery = trendList.join('|');
+    //console.log('After join: ' + youtubeQuery);
+
 });
 
 
@@ -54,15 +60,17 @@ function search() {
     var request = gapi.client.youtube.search.list({
         q: youtubeQuery,
         part: 'snippet',
-        maxResults: 20
+        maxResults: 10,
+        type: 'video'
     });
 
     request.execute(function(response) {
-        //console.log(response);
         var youtubeNumber = response.items.length;
 
         for (var i = 0; i < youtubeNumber; i++) {
+
             $('#list-container').append('<div class="col-sm-6 youtube-result"><a href="https://youtube.com//watch?v=' + response.items[i].id.videoId + '" target="_blank"><img src="'+ response.items[i].snippet.thumbnails.default.url +'" alt="" /><p>' + response.items[i].snippet.title + '</p></a></div>');
+
         }
     });
 }
